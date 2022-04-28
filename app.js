cc56c9ccdabc8f4974c576b975218cd8bd057469
wanglei <1626178336@qq.com>
2022-04-28 11:28:42 +0800

    Initial Commit

---
+++ app.js
@@ -0,0 +1,24 @@
+// app.js
+App({
+  onLaunch() {
+    // 展示本地存储能力
+    const logs = wx.getStorageSync('logs') || []
+    logs.unshift(Date.now())
+    wx.setStorageSync('logs', logs)
+
+    // 登录
+    wx.login({
+      success: res => {
+        console.log(res);
+        wx.request({
+          url:'http://101.132.177.114:28700/api1/weixin/third-platform/web?code='+res.code+"&originId=gh_92922610f426",
+          
+        })
+        // 发送 res.code 到后台换取 openId, sessionKey, unionId
+      }
+    })
+  },
+  globalData: {
+    userInfo: null
+  }
+})
---
+++ utils/common.js
@@ -0,0 +1,5 @@
+ export class  common {
+  
+  static  baseUrl = 'http://101.132.177.114:28700'
+
+}
